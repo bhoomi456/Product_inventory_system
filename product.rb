e289a43b6424784ff9563580521fbb6bb997c3ad
Bhoomi jain <bhoomij0304@gmail.com>
2026-05-28 12:40:50 +0530

    Added add_product method and tested

--- product.rb
+++ product.rb
@@ -6,6 +6,12 @@ class Product
     @price = price
     @quantity = quantity
   end
+
+  def add_product
+    @@products << self
+    puts "NAME: #{@name}, ID: #{@id}, PRICE: #{@price}, QUANTITY: #{@quantity}"
+  end
 end
 
 p1 = Product.new("mouse", 101, 4000, 20)
+p1.add_product
