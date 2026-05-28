d6215fc7ffc23fef84f27a0974d530a1e84dbf3d
Bhoomi jain <bhoomij0304@gmail.com>
2026-05-28 12:53:19 +0530

    added find product method and tested

--- product.rb
+++ product.rb
@@ -1,5 +1,6 @@
 class Product
   @@products = []
+  attr_accessor :name, :id, :price, :quantity
   def initialize(name, id, price, quantity)
     @name = name
     @id = id
@@ -11,7 +12,13 @@ class Product
     @@products << self
     puts "NAME: #{@name}, ID: #{@id}, PRICE: #{@price}, QUANTITY: #{@quantity}"
   end
+
+  def find_product(id)
+    @@products.find { |p| p.id == id}
+    puts "product #{@id}"
+  end
 end
 
 p1 = Product.new("mouse", 101, 4000, 20)
 p1.add_product
+p1.find_product(101)
